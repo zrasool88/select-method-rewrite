d9087e5e9e4fedc0bc0279e4e64d40c37810e1d4
Zeeshan Rasool <zrasool88@gmail.com>
2015-05-07 16:27:46 +0100

    iterative select method works

--- lib/array.rb
+++ lib/array.rb
@@ -1,3 +1,7 @@
 class Array
-
+  def myselect
+    new_array = []
+    each { |element| new_array << element if yield(element) == true }
+    new_array
+  end
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -2,5 +2,20 @@ require 'spec_helper'
 require 'array'
 
 describe Array do
+  context 'iterative select method' do
+    it 'selects numbers based on given condition' do
+      arr = [4, 8, 15, 16, 23, 42]
+      expect(arr.myselect { |number| number % 2 == 0 }).to eq([4, 8, 16, 42])
+    end
 
+    it 'selects strings based on given condition' do
+      arr1 = %w(I, Am, Pilgrim)
+      expect(arr1.myselect {|word| word.length > 3}).to eq(['Pilgrim'])
+    end
+
+    it 'selects an array from within an array based on a given condition' do
+      nested_arr = [[4, 8, 15, 16, 23, 42],%w(I, Am, Pilgrim)]
+      expect(nested_arr.myselect {|array| array.size > 3}).to eq([[4, 8, 15, 16, 23, 42]])
+    end
+  end
 end
