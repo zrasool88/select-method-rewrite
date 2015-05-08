34fab731168215cb68c7e35c1951abc49be3b27d
Zeeshan Rasool <zrasool88@gmail.com>
2015-05-08 01:21:40 +0100

    recursive select method works. now lets see if it can accept symbols as arguments. also it should return enum if no block is given. these two things left to implement

--- lib/array.rb
+++ lib/array.rb
@@ -4,4 +4,11 @@ class Array
     each { |element| new_array << element if yield(element) == true }
     new_array
   end
+
+  def myselect_recursive(new_array = [], &block)
+    copy_of_array = dup
+    return new_array if copy_of_array.empty?
+    new_array << copy_of_array[0] if yield(copy_of_array[0]) == true
+    copy_of_array.drop(1).myselect_recursive(new_array, &block)
+  end
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -5,17 +5,33 @@ describe Array do
   context 'iterative select method' do
     it 'selects numbers based on given condition' do
       arr = [4, 8, 15, 16, 23, 42]
-      expect(arr.myselect { |number| number % 2 == 0 }).to eq([4, 8, 16, 42])
+      expect(arr.myselect { |number| number.even? }).to eq([4, 8, 16, 42])
     end
 
     it 'selects strings based on given condition' do
       arr1 = %w(I, Am, Pilgrim)
-      expect(arr1.myselect {|word| word.length > 3}).to eq(['Pilgrim'])
+      expect(arr1.myselect { |word| word.length > 3 }).to eq(['Pilgrim'])
     end
 
     it 'selects an array from within an array based on a given condition' do
-      nested_arr = [[4, 8, 15, 16, 23, 42],%w(I, Am, Pilgrim)]
+      nested_arr = [[4, 8, 15, 16, 23, 42], %w(I, Am, Pilgrim)]
       expect(nested_arr.myselect {|array| array.size > 3}).to eq([[4, 8, 15, 16, 23, 42]])
+    end
+  end
+
+  context 'recursive select method' do
+    it 'selects numbers based on given condition' do
+      arr = [4, 8, 15, 16, 23, 42]
+      expect(arr.myselect_recursive { |number| number.even? }).to eq([4, 8, 16, 42])
+    end
+
+    it 'selects strings based on given condition' do
+      arr1 = %w(I, Am, Pilgrim)
+      expect(arr1.myselect_recursive {|word| word.length > 3}).to eq(['Pilgrim'])
+    end
+    it 'selects an array from within an array based on a given condition' do
+      nested_arr = [[4, 8, 15, 16, 23, 42], %w(I, Am, Pilgrim)]
+      expect(nested_arr.myselect_recursive {|array| array.size > 3}).to eq([[4, 8, 15, 16, 23, 42]])
     end
   end
 end
