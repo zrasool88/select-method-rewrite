bb55ecd0d37b1c750a8182fe8de7f74c28b7ef69
Zeeshan Rasool <zrasool88@gmail.com>
2015-05-08 01:32:37 +0100

    method works with &:methodname syntax

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -5,33 +5,52 @@ describe Array do
   context 'iterative select method' do
     it 'selects numbers based on given condition' do
       arr = [4, 8, 15, 16, 23, 42]
-      expect(arr.myselect { |number| number.even? }).to eq([4, 8, 16, 42])
+      expect(arr.myselect do |number|
+        number.even?
+      end).to eq([4, 8, 16, 42])
     end
 
     it 'selects strings based on given condition' do
-      arr1 = %w(I, Am, Pilgrim)
+      arr1 = %w(I Am Pilgrim)
       expect(arr1.myselect { |word| word.length > 3 }).to eq(['Pilgrim'])
     end
 
     it 'selects an array from within an array based on a given condition' do
-      nested_arr = [[4, 8, 15, 16, 23, 42], %w(I, Am, Pilgrim)]
-      expect(nested_arr.myselect {|array| array.size > 3}).to eq([[4, 8, 15, 16, 23, 42]])
+      nested_arr = [[4, 8, 15, 16, 23, 42], %w(I Am Pilgrim)]
+      expect(nested_arr.myselect do |array|
+        array.size > 3
+      end).to eq([[4, 8, 15, 16, 23, 42]])
+    end
+    
+    it 'selects numbers based on given condition' do
+      arr = [4, 8, 15, 16, 23, 42]
+      expect(arr.myselect(&:odd?)).to eq([15, 23])
     end
   end
 
   context 'recursive select method' do
     it 'selects numbers based on given condition' do
       arr = [4, 8, 15, 16, 23, 42]
-      expect(arr.myselect_recursive { |number| number.even? }).to eq([4, 8, 16, 42])
+      expect(arr.myselect_recursive do |number|
+        number.even?
+      end).to eq([4, 8, 16, 42])
     end
 
     it 'selects strings based on given condition' do
-      arr1 = %w(I, Am, Pilgrim)
-      expect(arr1.myselect_recursive {|word| word.length > 3}).to eq(['Pilgrim'])
+      arr1 = %w(I Am Pilgrim)
+      expect(arr1.myselect_recursive { |word| word.length == 2 }).to eq(['Am'])
     end
+    
     it 'selects an array from within an array based on a given condition' do
-      nested_arr = [[4, 8, 15, 16, 23, 42], %w(I, Am, Pilgrim)]
-      expect(nested_arr.myselect_recursive {|array| array.size > 3}).to eq([[4, 8, 15, 16, 23, 42]])
+      nested_arr = [[4, 8, 15, 16, 23, 42], %w(I Am Pilgrim)]
+      expect(nested_arr.myselect_recursive do |array|
+        array.size == 3
+      end).to eq([%w(I Am Pilgrim)])
+    end
+
+    it 'selects numbers based on given condition' do
+      arr = [4, 8, 15, 16, 23, 42]
+      expect(arr.myselect_recursive(&:odd?)).to eq([15, 23])
     end
   end
 end
